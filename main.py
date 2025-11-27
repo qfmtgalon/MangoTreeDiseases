5454a5a6bf15fb6001ce7ddc021385e2633a4233
Faith Mary Galon <faithmarygalon.fmg@gmail.com>
2025-11-28 03:19:54 +0800

    test

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Msg(BaseModel):
 
 @app.get("/anthracnose")
 async def root():
-    return {"DESCRIPTION": "Antracnose is a fungal disease that affects a wide range of plants, including mango trees. It is caused by several species of fungi in the Colletotrichum genus, which can infect the leaves, stems, flowers, and fruit of the tree. Symptoms of antracnose on mango trees include small, dark-colored spots on the leaves, stems, and fruit.", "LETHALITY": "Not lethal, moderate", "HUMIDITY": "Warm and moist weather"}
+    return {"DESCRIPTION": "sss", "LETHALITY": "Not lethal, moderate", "HUMIDITY": "Warm and moist weather"}
 
 
 @app.get("/sootymold")
